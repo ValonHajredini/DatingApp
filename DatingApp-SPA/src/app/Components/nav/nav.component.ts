e3512d182375e229c10bbbfea26cf441ed903d9c
Valon Hajredini <hajredini.valon@gmail.com>
2019-05-19 21:45:31 +0200

    Error handling interceptor

--- DatingApp-SPA/src/app/Components/nav/nav.component.ts
+++ DatingApp-SPA/src/app/Components/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Loged In Sucesfully')
     }, error => {
-      console.log('Faild to login')
+      console.log(error)
     });
   }
 
